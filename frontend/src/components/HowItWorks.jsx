24022426c50e8e365b3de664987ed0777d4414bc
Randy T <randythai360@gmail.com>
2025-07-20 15:36:06 -0500

    put iframe into VideoPiece component

--- frontend/src/components/HowItWorks.jsx
+++ frontend/src/components/HowItWorks.jsx
@@ -1,4 +1,4 @@
-import { CirclePlay } from "lucide-react";
+import { CirclePlay, Video } from "lucide-react";
 import { easeInOut, motion, useInView } from "framer-motion";
 import { useRef, useState } from "react";
 
@@ -6,17 +6,6 @@ export default function HowItWorks() {
     
     const VideoPiece = ({ videoSrc, title, description }) => {
         const videoRef = useRef(null);
-        // const [playing, setPlaying] = useState(playOnStart);
-    
-        // const playVideo = () => {
-        //     setPlaying(true);
-        //     videoRef.current.play();
-        // };
-    
-        // const pauseVideo = () => {
-        //     setPlaying(false);
-        //     videoRef.current.pause();
-        // };
         
         return (
             <div className="rounded-xl bg-stone-200 flex justify-center p-8">
@@ -27,20 +16,13 @@ export default function HowItWorks() {
                     viewport={{ once: true }}
                     transition={{ duration: 0.8, ease: "easeInOut" }}
                     className="relative flex">
-
-                        {/* {!playing && (
-                            <>
-                                <div className="absolute w-full h-full rounded-xl bg-black opacity-50 z-10" />
-                                <button className="flex justify-center items-center absolute w-full h-full rounded-full z-20 text-white" onClick={playVideo}><CirclePlay className="w-[40px] h-[40px]" /></button>
-                            </>
-                        )} */}
-                        <img
-                        // autoPlay={playOnStart}
-                        // ref={videoRef}
-                        className="rounded-xl"
-                        src={videoSrc} alt="demo video" loop muted playsInline
-                        // onClick={pauseVideo} 
-                        />
+                        <iframe 
+                        className="absolute top-0 left-0 w-full h-full border border-stone-300 rounded-xl z-10"
+                        src={videoSrc}
+                        title="YouTube video player" 
+                        allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin" 
+                        ></iframe>
 
                     </motion.div>
                     <p className="font-bold py-2 pt-6 text-stone-700">{title}</p>
@@ -88,15 +70,12 @@ export default function HowItWorks() {
                 </motion.div>
                 <div className="w-full grid md:grid-cols-2 gap-10 px-20">
                     {/* Demo Video - Save and view carts from the extension */}
-                    <div className="relative w-full" style={{ paddingBottom: "100%" }}>
-                        <iframe 
-                        className="absolute top-0 left-0 w-full h-full border border-stone-300 rounded-xl z-10"
-                        src="https://www.youtube-nocookie.com/embed/YqYcIHZ3Egs?autoplay=1&mute=1&controls=0&rel=0&loop=1&playlist=YqYcIHZ3Egs"
-                        title="YouTube video player" 
-                        allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerPolicy="strict-origin-when-cross-origin" 
-                        ></iframe>
-                    </div>
+                    <VideoPiece videoSrc="https://www.youtube-nocookie.com/embed/YqYcIHZ3Egs?autoplay=1&mute=1&controls=0&rel=0&loop=1&playlist=YqYcIHZ3Egs"
+                    title="All Your Carts in One Place"
+                    description="Keep track of every item you save—no matter the store. 
+                    Seamlessly view and manage products from across the web in one 
+                    convenient space."
+                    />
 
                     <VideoPiece videoSrc="/videos/CreateAndDeleteCart.gif" 
                     title="Create and Delete Carts" 
