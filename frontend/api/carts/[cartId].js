2068b53c9e0a12eb01c5763af2884ca4c72101d1
dhruvchowdhary <dhruvchowdhary@berkeley.edu>
2026-01-08 23:09:25 -0800

    fix API routes for vercel, should fix cart edit

--- frontend/api/carts/[cartId].js
+++ frontend/api/carts/[cartId].js
@@ -3,8 +3,10 @@ const { verifyToken } = require("../_lib/auth");
 
 console.log("[cartId] hit");
 module.exports = async function handler(req, res) {
-  console.log(`[cartId] API called with method: ${req.method}, cartId: ${req.query.cartId}`);
-  
+  console.log(
+    `[cartId] API called with method: ${req.method}, cartId: ${req.query.cartId}`
+  );
+
   // Enable CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "PUT, DELETE, OPTIONS");
